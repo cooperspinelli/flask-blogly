d7d447da2e6a241dcbed978d924e077464815a64
David Sapiro <dsap48@gmail.com>
2024-02-28 12:28:51 -0800

    table structure and /users route

--- app.py
+++ app.py
@@ -2,13 +2,23 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, render_template
+from models import connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secretsecret"
 
 connect_db(app)
+
+@app.get('/users')
+def list_users():
+    """List users with an add button."""
+
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
