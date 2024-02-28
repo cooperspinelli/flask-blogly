6b9f453668a3b083210ed3fbb19031260dd970d4
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-28 13:04:52 -0700

    Add model for users table

--- models.py
+++ models.py
@@ -1,1 +1,37 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to the database"""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    first_name = db.Column(
+        db.String(25),
+        nullable = False
+    )
+
+    last_name = db.Column(
+        db.String(25),
+        nullable = False
+    )
+
+    image_url = db.Column(
+        db.String,
+        nullable = True
+    )
