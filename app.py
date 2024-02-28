76e313d2cec10e05ee6395ab73d0add000ac4d5b
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-28 16:10:39 -0700

    Add endpoints for creating new user

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask import Flask, render_template
-from models import connect_db, User
+from flask import Flask, render_template, redirect, request
+from models import connect_db, User, db
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -15,6 +15,13 @@ app.config['SECRET_KEY'] = "secretsecret"
 
 connect_db(app)
 
+
+@app.get('/')
+def redirect_to_users():
+    """Redirects to users page"""
+
+    return redirect('/users')
+
 @app.get('/users')
 def list_users():
     """List users with an add button."""
@@ -22,3 +29,26 @@ def list_users():
     users = User.query.all()
     return render_template("users.html", users=users)
 
+
+@app.get('/users/new')
+def new_user_form():
+    """Returns form to create new user"""
+
+    return render_template('new_user.html')
+
+
+@app.post('/users/new')
+def create_new_user():
+
+    first = request.form['first']
+    last = request.form['last']
+    url = request.form['image_url']
+
+    new_user = User(first_name = first,
+                    last_name = last,
+                    image_url = url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
